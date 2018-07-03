3441da02af639d5628b6b5f594165e51b55e37f1
Danica Priest <Danicamaypriest@hotmail.co.uk>
2018-07-03 15:35:31 +0100

    removing iml

--- src/main/java/AnaylzeMyFace/Model/DetectedFaces.java
+++ src/main/java/AnaylzeMyFace/Model/DetectedFaces.java
@@ -1,4 +1,13 @@
 package AnaylzeMyFace.Model;
 
 public class DetectedFaces {
+    Images[] images;
+
+    public Images[] getImages() {
+        return images;
+    }
+
+    public void setImages(Images[] images) {
+        this.images = images;
+    }
 }
