4d9437f092f9e0ba48cd49c06b732718f8c232e1
Danica Priest <Danicamaypriest@hotmail.co.uk>
2018-07-04 23:14:37 +0100

    adding mvc

--- src/main/java/AnaylzeMyFace/Controller/AMFController.java
+++ src/main/java/AnaylzeMyFace/Controller/AMFController.java
@@ -6,16 +6,24 @@ import com.ibm.watson.developer_cloud.http.ServiceCall;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectFacesOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("analyzemyface/")
 public class AMFController {
     @Autowired
     AMFService amfService;
-
     @RequestMapping("/")
+    public ModelAndView home(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+        return modelAndView;
+    }
+
+    @RequestMapping("json/")
     public com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces faceFacts(@RequestParam(value="url") String url){
         return  amfService.faceFacts(url);
     }
